c9e661ea7ca5609f3c5e5722f4a87f682bb3ddb4
AlonsoAp <zomboy.alonso@gmail.com>
2022-04-28 02:14:15 -0500

    Update: controller response create Provider

--- src/controllers/providers.controller.js
+++ src/controllers/providers.controller.js
@@ -1,59 +1,66 @@
-import Provider from "../models/Provider"
-import Document from "../models/Document"
+import Provider from "../models/Provider";
+import Document from "../models/Document";
 
 export const createProvider = async (req, res) => {
-    const {
-        businessName,
-        contactName,
-        phoneNumber,
-        status,
-        documentNumber,
-        documentType
-    } = req.body;
+  const {
+    businessName,
+    contactName,
+    phoneNumber,
+    status,
+    documentNumber,
+    documentType,
+  } = req.body;
 
-    const newProvider = new Provider({
-        businessName,
-        contactName,
-        phoneNumber,
-        status,
-        documentNumber
-    })
+  const newProvider = new Provider({
+    businessName,
+    contactName,
+    phoneNumber,
+    status,
+    documentNumber,
+  });
 
-    const foundDocuments = await Document.find({name: {$in: documentType}})
+  const foundDocuments = await Document.find({ name: { $in: documentType } });
 
-    if (!foundDocuments.length>0) return res.status(400).json({message: "Documento no encontrado"})
+  if (!foundDocuments.length > 0)
+    return res.status(400).json({ message: "Documento no encontrado" });
 
-    newProvider.documentType = foundDocuments[0];
+  newProvider.documentType = foundDocuments[0];
 
-    const savedProvider = await newProvider.save();
+  const savedProvider = await newProvider.save();
 
-    res.status(201).json(savedProvider)
-}
+  res.status(201).json({ status: 201, savedProvider });
+};
 
 export const getProviders = async (req, res) => {
-    const providers = await Provider.find().populate('documentType');
-    res.json(providers)
-}
+  const providers = await Provider.find().populate("documentType");
+  res.json(providers);
+};
 
 export const getProviderById = async (req, res) => {
-    try {
-        const provider = await Provider.findById(req.params.providerId).populate('documentType');
-        res.status(200).json(provider)
-    } catch (error) {
-        res.status(400).json({message: "Proveedor no encontrado"});
-    }
-}
+  try {
+    const provider = await Provider.findById(req.params.providerId).populate(
+      "documentType"
+    );
+    res.status(200).json(provider);
+  } catch (error) {
+    res.status(400).json({ message: "Proveedor no encontrado" });
+  }
+};
 
 export const updateProviderById = async (req, res) => {
-    try {
-        const updatedprovider = await Provider.findByIdAndUpdate(req.params.providerId, req.body, {
-            new: true
-        })
-        res.status(200).json({status:200, updatedprovider})
-    } catch (error) {
-        res.status(400).json({message: "No se actualizó el proveedor"});
-    }
-}
+  try {
+    const updatedProvider = await Provider.findByIdAndUpdate(
+      req.params.providerId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({ status: 200, updatedProvider });
+  } catch (error) {
+    res.status(400).json({ message: "No se actualizó el proveedor" });
+  }
+};
 
 // export const deleteProviderById = async (req, res) => {
 //     const {providerId} = req.params;
