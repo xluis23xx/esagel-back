f6f0888d54cd2cd92a6e2770712ef6ecc4beccdc
AlonsoAp <zomboy.alonso@gmail.com>
2022-05-01 00:26:27 -0500

    Update: ajuste en settings para que devuelva el primer resultado y imagen por defecto de esagel

--- src/controllers/settings.controller.js
+++ src/controllers/settings.controller.js
@@ -1,20 +1,23 @@
-import Setting from "../models/Setting"
+import Setting from "../models/Setting";
 
-export const getSettings = async(req, res) => {
-    const settings = await Setting.find();
-    res.json(settings)
-}
+export const getSettings = async (req, res) => {
+  const settings = await Setting.find();
+  res.json(settings[0]);
+};
 
 export const updateSettingById = async (req, res) => {
-    try {
-        const updateSetting = await Setting.findByIdAndUpdate(
-            req.params.settingId, 
-            req.body, {
-                new: true
-            }
-        )
-        res.status(200).json({ status: 200, updateSetting })
-    } catch (error) {
-        res.status(400).json({ status: 400, message: 'No se actualizó las configuraciones' });
-    }
-}
+  try {
+    const updateSetting = await Setting.findByIdAndUpdate(
+      req.params.settingId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({ status: 200, updateSetting });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: 400, message: "No se actualizó las configuraciones" });
+  }
+};
--- src/libs/initialSetup.js
+++ src/libs/initialSetup.js
@@ -1,70 +1,84 @@
-import Role from '../models/Role'
-import Document from '../models/Document'
-import Setting from '../models/Setting'
+import Role from "../models/Role";
+import Document from "../models/Document";
+import Setting from "../models/Setting";
 
-export const createRoles = async() => {
-    try {
-        const count = await Role.estimatedDocumentCount()
+export const createRoles = async () => {
+  try {
+    const count = await Role.estimatedDocumentCount();
 
-        if (count>0) return;
+    if (count > 0) return;
 
-        const values = await Promise.all([
-            new Role({name: 'user'}).save(),
-            new Role({name: 'moderator'}).save(),
-            new Role({name: 'admin'}).save()
-        ])
+    const values = await Promise.all([
+      new Role({ name: "user" }).save(),
+      new Role({ name: "moderator" }).save(),
+      new Role({ name: "admin" }).save(),
+    ]);
 
-        console.log(values);
-    } catch (error) {
-        console.error(values);
-    }
-}
+    console.log(values);
+  } catch (error) {
+    console.error(values);
+  }
+};
 
-export const createDocuments = async() => {
-    try {
-        const count = await Document.estimatedDocumentCount()
+export const createDocuments = async () => {
+  try {
+    const count = await Document.estimatedDocumentCount();
 
-        if (count>0) return;
+    if (count > 0) return;
 
-        const values = await Promise.all([
-            new Document({name: 'DNI', operation: 'persona', status: 1 }).save(),
-            new Document({name: 'RUC', operation: 'persona', status: 1}).save(),
-            new Document({name: 'CIP', operation: 'persona', status: 1}).save(),
-            new Document({name: 'Carnet Extranjeria', operation: 'persona', status: 1}).save(),
-            new Document({name: 'Pasaporte', operation: 'persona', status: 1}).save(),
-            new Document({name: 'CPP', operation: 'persona', status: 1}).save(),
-            new Document({name: 'Boleta', operation: 'comprobante', status: 1}).save(),
-            new Document({name: 'Factura', operation: 'comprobante', status: 1}).save(),
-        ])
+    const values = await Promise.all([
+      new Document({ name: "DNI", operation: "persona", status: 1 }).save(),
+      new Document({ name: "RUC", operation: "persona", status: 1 }).save(),
+      new Document({ name: "CIP", operation: "persona", status: 1 }).save(),
+      new Document({
+        name: "Carnet Extranjeria",
+        operation: "persona",
+        status: 1,
+      }).save(),
+      new Document({
+        name: "Pasaporte",
+        operation: "persona",
+        status: 1,
+      }).save(),
+      new Document({ name: "CPP", operation: "persona", status: 1 }).save(),
+      new Document({
+        name: "Boleta",
+        operation: "comprobante",
+        status: 1,
+      }).save(),
+      new Document({
+        name: "Factura",
+        operation: "comprobante",
+        status: 1,
+      }).save(),
+    ]);
 
-        console.log(values);
-    } catch (error) {
-        console.log(values);
-    }
-}
+    console.log(values);
+  } catch (error) {
+    console.log(values);
+  }
+};
 
-export const createSetting = async() => {
-    try {
-        const count = await Setting.estimatedDocumentCount()
+export const createSetting = async () => {
+  try {
+    const count = await Setting.estimatedDocumentCount();
 
-        if (count>0) return;
+    if (count > 0) return;
 
-        const values = await Promise.all([
-            new Setting(
-                {
-                    companyName: 'ESAGEL', 
-                    description: 'Esagel Trujillo', 
-                    businessName: 'ESAGEL S.A.C',
-                    ruc: '12345678911',
-                    url: 'https://www.facebook.com/groups/grupoesagel',
-                    logo: '/logo',
-                    tax: 0.18
-                }
-            ).save()
-        ])
+    const values = await Promise.all([
+      new Setting({
+        companyName: "ESAGEL",
+        description: "Esagel Trujillo",
+        businessName: "ESAGEL S.A.C",
+        ruc: "12345678911",
+        url: "https://www.facebook.com/groups/grupoesagel",
+        logo: "https://firebasestorage.googleapis.com/v0/b/restaurant-app-6a09d.appspot.com/o/saegel%2Fesagel.png?alt=media&token=04d99e71-7eea-4876-8a44-6d1a4dc4f330",
+        tax: 0.18,
+      }).save(),
+    ]);
 
-        console.log(values);
-    } catch (error) {
-        console.log(values);
-    }
-} 
+    console.log(values);
+  } catch (error) {
+    console.log(values);
+  }
+};
