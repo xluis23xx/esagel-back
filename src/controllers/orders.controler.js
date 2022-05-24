4a4ea4cc468d0e54eab60bd9159ecd2d54d2deff
xluis23xx <luis_alexander_23_05_96@hotmail.com>
2022-05-24 01:27:07 -0500

    First version api orders

--- src/controllers/orders.controler.js
+++ src/controllers/orders.controler.js
@@ -40,69 +40,56 @@ export const createOrder = async (req, res) => {
         .status(400)
         .json({ status: 400, message: "Tipo documento no encontrado" });
 
-    // console.log(" orderLines", orderLines[0].course)
-    // const foundCourse = await Course.findOne({ code: { $in: "PER" } });
-    // console.log("foundCourse",foundCourse);
-    // orderLines.map((element)=>{
-    //     console.log("elementMap",element);
-    //     const foundCourse = await Course.findOne({ code: { $in: element.course } });
-    // })
-    // orderLines.forEach((element) => {
-    //     console.log("element",element);
-    //     // const item = await findCourse(element);
-    //     console.log("item",item.name);
-    //     // const foundCourse = await Course.findOne({ code: { $in: element.course } });
-    //     // console.log("element",foundCourse);
-    // //   try {
-    //     //   console.log("element",element.course);
-    //     // const foundCourses = Course.find({ code: { $in: "PER" } });
-    //     // console.log("foundCourses",foundCourses.name);
+    await Promise.all(
+      orderLines.map(async (element) => {
+        try {
+          const foundCourses = await Course.find({
+            code: { $in: element.course },
+          });
+  
+          if (!foundCourses.length > 0)
+            return res
+              .status(400)
+              .json({ status: 400, message: "Curso no encontrado" });
+  
+          const newOrderLine = new OrderDetail({
+            name: element.name,
+            quantity: element.quantity,
+            discount: element.discount,
+            price: element.price,
+            amount: element.amount,
+            course: foundCourses[0],
+          });
+  
+          await newOrderLine.save().then(savedOrderLine => {
+            collectionLines.push(newOrderLine);
+          });  
+        } catch (error) {
+          res
+            .status(400)
+            .json({ status: 400, message: "No se creó el detalle del pedido" });
+        }
+      })
+    )
 
-    //     // if (!foundCourses.length > 0)
-    //     //   return res
-    //     //     .status(400)
-    //     //     .json({ status: 400, message: "Curso no encontrado" });
-        
-    //     // console.log("foundCourses",foundCourses);
+    const newOrder = new Order({
+      orderNumber,
+      percentIva,
+      subtotal,
+      amountInIva,
+      total,
+      status: status ? status : 1,
+      seller: foundSellers[0],
+      client: foundClients[0],
+      documentType: foundDocuments[0],
+      documentNumber,
+      orderLines: collectionLines,
+    });
 
-    //     // const newOrderLine = new OrderDetail({
-    //     //   name: element.name,
-    //     //   quantity: element.quantity,
-    //     //   discount: element.discount,
-    //     //   price: element.price,
-    //     //   amount: element.amount,
-    //     //   course: foundCourses[0],
-    //     // });
+    const savedOrder = await newOrder.save();
 
-    //     // const savedOrderLine = newOrderLine.save();
-    //     // collectionLines.push(savedOrderLine);
-    // //   } catch (error) {
-    // //     res
-    // //       .status(400)
-    // //       .json({ status: 400, message: "Error al crear la línea del pedido" });
-    // //   }
-    // });
-
-    // console.log("collectionLines", collectionLines);
-    // const newOrder = new Order({
-    //   orderNumber,
-    //   percentIva,
-    //   subtotal,
-    //   amountInIva,
-    //   total,
-    //   status: status ? status : 1,
-    //   seller: foundSellers[0],
-    //   client: foundClients[0],
-    //   documentType: foundDocuments[0],
-    //   documentNumber,
-    //   orderLines: collectionLines,
-    // });
-
-    // const savedOrder = await newOrder.save();
-
-    // res.status(201).json({ status: 201, savedOrder });
+    res.status(201).json({ status: 201, savedOrder });
   } catch (error) {
-    res.status(400).json({ status: 400, message: "No se creó el pedido" });
+    res.status(400).json({ status: 400, message: error });
   }
 };
-  
