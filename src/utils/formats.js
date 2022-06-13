62b7015aadb2e6d16340f6625ddb7474a8df4c30
Pablo Urbano <pablojamiro2008@hotmail.com>
2022-06-12 22:39:52 -0500

    Update: util agregado

---
+++ src/utils/formats.js
@@ -0,0 +1,25 @@
+export const setFormatDate = ({ order = 0, date, separator = "-" }) => {
+  if (date) {
+    const convertDate = date ? new Date(date) : "";
+    const year = convertDate ? convertDate.getFullYear() : "";
+    let month = convertDate ? convertDate.getMonth() + 1 : "";
+    if (month < 10) {
+      month = `0${month}`;
+    }
+    let day = convertDate ? convertDate.getDate() : "";
+    if (day < 10) {
+      day = `0${day}`;
+    }
+    if (order === 0) {
+      return year && month && day
+        ? `${day}${separator}${month}${separator}${year}`
+        : "";
+    }
+    if (order === 1) {
+      return year && month && day
+        ? `${year}${separator}${month}${separator}${day}`
+        : "";
+    }
+  }
+  return "";
+};
