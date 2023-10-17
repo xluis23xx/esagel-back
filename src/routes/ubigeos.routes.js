2841cacc954005d82ee7728b03f3eccfa582d720
xluis23xx <luis_alexander_23_05_96@hotmail.com>
2023-10-17 00:29:49 -0500

    update ubigeo

--- src/routes/ubigeos.routes.js
+++ src/routes/ubigeos.routes.js
@@ -4,6 +4,6 @@ const router = Router()
 import * as ubigeoCtrl from '../controllers/ubigeo.controller';
 import { authJwt} from '../middlewares';
 
-router.post('/ubigeo', [authJwt.verifyToken, authJwt.isUser], ubigeoCtrl.getUbigeo)
+router.get('/ubigeo', [authJwt.verifyToken, authJwt.isUser], ubigeoCtrl.getUbigeo)
 
 export default router;
