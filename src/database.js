448c756391ea7b89994c3b65c7824c75c5bb0304
xluis23xx <luis_alexander_23_05_96@hotmail.com>
2023-10-18 03:22:38 -0500

    update files env

--- src/database.js
+++ src/database.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose'
 
-const db = mongoose.connect(process.env.CONNECT_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
+const db = ()=> { 
+    mongoose.connect(
+        process.env.CONNECT_DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        })
     .then(db => console.log('Db is connected'))
     .catch(error => console.log(error))
+}
 
 export default db
